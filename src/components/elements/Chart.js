421fae15110e325629608ad14cf54bbb33a36743
Evander Oktapian <evandertambunan@gmail.com>
2023-11-08 23:26:49 +0700

    add chart component

---
+++ src/components/elements/Chart.js
@@ -0,0 +1,44 @@
+import { Line, Bar, Doughnut, Pie } from "react-chartjs-2";
+import { Chart as ChartJS, BarElement, LineElement, ArcElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(BarElement, LineElement, ArcElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend);
+
+const Chart = ({ type, labels, title, value, color }) => {
+    let colorRandom = ["#FFC0CB","#AED6F1","#98FB98","#FFD700","#B19CD9","#D3D3D3"]
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: title,
+                data: value,
+                backgroundColor: (type==="Line"||type==="Bar")?color :colorRandom,
+                borderColor: (type==="Line"||type==="Bar")?color :"white" ,
+                pointBorderColor: "black",
+                fill: true,
+                tension: 0.3,
+            },
+        ],
+    };
+    const options = {
+        plugin: {
+            legend: true,
+        },
+        scales: {
+            y: {
+                min: value.reduce((a,b)=>Math.min(a,b)),
+                max: Math.ceil(value.reduce((a,b)=>Math.max(a,b))*1.1),
+            },
+        },
+    };
+    return (
+        <>
+            <h1>Ini adalah Chart</h1>
+            {type === "Line" && <Line data={data} options={options}></Line>}
+            {type === "Bar" && <Bar data={data} options={options}></Bar>}
+            {type === "Doughnut" && <Doughnut data={data} options={options}></Doughnut>}
+            {type === "Pie" && <Pie data={data} options={options}></Pie>}
+        </>
+    );
+};
+
+export default Chart;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
-
+import Chart from "@/components/elements/Chart";
 
 export default function Home() {
     return (
         <>
-        <h1>Landing Page</h1>
+            <h1>Landing Page</h1>
+            <div style={{ width: "500px" }}>
+                <Chart type={"Bar"} labels={["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul"]} title={"Income"} value={[10, 15, 12, 0, 8, 10, 20]} color={"grey"} />
+            </div>
         </>
     );
 }
