6e655716dfd178cf2e26a3ff4eb280a7050350f7
Evander Oktapian <evandertambunan@gmail.com>
2023-11-23 17:23:26 +0700

    check wallet page

--- src/pages/wallet/index.js
+++ src/pages/wallet/index.js
@@ -1,74 +1,80 @@
 import { Row, Col, Spinner, Alert, Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import { getSaldoMonthly, getWalletSaldo } from "@/rest_API/wallets_api";
+import { getWalletSaldo } from "@/rest_API/wallets_api";
 import WalletSummaryContent from "@/components/elements/WalletSummaryContent";
 import { AiFillBank } from "react-icons/ai";
 import Graphic from "@/components/fragments/Graphic";
-import ModalWallet from "@/components/elements/ModalWallet"
+import ModalWallet from "@/components/elements/ModalWallet";
+import Chart from "@/components/elements/Chart";
 const Wallet = () => {
-  const [wallets, setWallets] = useState([]);
-  const [monthlySaldoData, setMonthlySaldoData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [wallets, setWallets] = useState([]);
+    const [monthlySaldoData, setMonthlySaldoData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [chartData,setChartData] = useState([])
 
-  useEffect(() => {
+    useEffect(() => {
+      
+      fetchData();
+    }, []);
     const fetchData = async () => {
-      try {
-        const walletResponse = await getWalletSaldo();
-        setWallets(walletResponse);
+        try {
+            const walletResponse = await getWalletSaldo();
+            console.log(walletResponse);
+            setWallets(walletResponse);
 
-        const monthlySaldoResponse = await getSaldoMonthly();
-        setMonthlySaldoData(monthlySaldoResponse);
-      } catch (error) {
-        setError("Error fetching wallet data");
-      } finally {
-        setLoading(false);
-      }
+            const filteredDataForChart = walletResponse.map(({wallet_id,saldo})=>({category:wallet_id, value : saldo}))
+            setChartData(filteredDataForChart)
+
+            // const monthlySaldoResponse = await getSaldoMonthly();
+            // setMonthlySaldoData(monthlySaldoResponse);
+        } catch (error) {
+            setError("Error fetching wallet data");
+        } finally {
+            setLoading(false);
+        }
     };
 
-    fetchData();
-  }, []);
-
-  return (
-    <Row>
-      <Col>
-        {loading ? (
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
-        ) : error ? (
-          <Alert variant="danger">{error}</Alert>
-        ) : wallets.length === 0 ? (
-          <p>No wallet data available.</p>
-        ) : (
-          <Row xs={1} md={2} lg={3} className="g-4 mt-1">
-            {wallets.map((wallet) => (
-              <Col key={wallet.id}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>{wallet.category}</Card.Title>
-                    <Card.Text>
-                      <WalletSummaryContent
-                        value={wallet.saldo}
-                        icon={<AiFillBank />}
-                      />
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        )}
-        <Col>
-          <div>
+    return (
+        <Row>
+            <Col>
+                {loading ? (
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                ) : error ? (
+                    <Alert variant="danger">{error}</Alert>
+                ) : wallets && wallets.length === 0 ? (
+                    <p>No wallet data available.</p>
+                ) : (
+                    <Row xs={1} md={2} lg={3} className="g-4 mt-1">
+                        {wallets.length > 0 ? (
+                            wallets.map((wallet) => (
+                                <Col key={wallet.id}>
+                                    <Card>
+                                        <Card.Body>
+                                            <Card.Title>{wallet.category}</Card.Title>
+                                            <Card.Text>
+                                                <WalletSummaryContent value={wallet.saldo} icon={<AiFillBank />} />
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            ))
+                        ) : (
+                            <p>No wallets available</p>
+                        )}
+                    </Row>
+                )}
+                <Col>
+                    <div>
             
             <h1>Saldo Monthly</h1>
             <ModalWallet></ModalWallet>
             <div style={{ width: '80%', height: '80%' }}>
-               <Graphic
+               <Chart
                 type={"Line"}
-                labels={monthlySaldoData.map((entry) => entry.month)}
-                value={monthlySaldoData.map((entry) => entry.saldo)}
+                datas = {chartData}
                 title={"Monthly Saldo"}
                 color={"green"}
                 width="80%"
@@ -77,10 +83,10 @@ const Wallet = () => {
               
             </div>
           </div>
-        </Col>
-      </Col>
-    </Row>
-  );
+                </Col>
+            </Col>
+        </Row>
+    );
 };
 
 export default Wallet;
--- src/rest_API/wallets_api.js
+++ src/rest_API/wallets_api.js
@@ -2,41 +2,44 @@ import axios from "axios";
 
 export const getWalletSaldo = async () => {
   try {
-    const response = await axios.post(
-      "http://localhost:3000/api/wallet/v1/walletsaldo",
+    const response = await axios.get(
+      "http://localhost:3001/api/wallet/v1/walletsaldo",
       {
-        user_id: 1,
+        params: {
+          user_id: 1,
+        },
       }
-    );
+    );    
     return response.data.data
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 };
-export const getSaldoMonthly = async () =>{
-    try {
-        const response = await axios.post(
-            "http://localhost:3000/api/wallet/v1/saldomonthly",
-            {
-              user_id: 1,
-            }
-        );
-        return response.data.data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-}
+// export const getSaldoMonthly = async () =>{
+//     try {
+//         const response = await axios.post(
+//             "http://localhost:3000/api/wallet/v1/saldomonthly",
+//             {
+//               user_id: 1,
+//             }
+//         );
+//         return response.data.data;
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//     }
+// }
 
-export const createWallet = async(user_id,category,description) => {
-    try {
-        const response = await axios.post(
-            "http://localhost:3000/api/wallet/v1/add",
-            {
-                user_id  , category, description
-            }
-        )
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-}
+// export const createWallet = async(user_id,category,description) => {
+//     try {
+//         const response = await axios.post(
+//             "http://localhost:3000/api/wallet/v1/add",
+//             {
+//                 user_id  , category, description
+//             }
+//         )
+//         return response.data;
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//     }
+// }
+
