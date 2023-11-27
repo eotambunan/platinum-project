f5d9d2d042b7740fc71fb875bb1b8883e2a2edad
Rolland <rsadiraaksyana@gmail.com>
2023-11-27 14:21:33 +0700

    revised login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,72 +1,66 @@
-// login.js
-
-import { loginApi, registerApi } from "@/rest_API/users_api";
+import { useState } from "react";
+import { loginApi } from "@/rest_API/users_api";
+import styles from "./login.module.css"; // Make sure the file path is correct
 import { setCookie } from "@/utils/cookies";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import styles from "./login.module.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [isLoginMode, setLoginMode] = useState(true); // Added state for mode
+  const [isLogin, setIsLogin] = useState(true); // Use this state to switch between login and registration forms
+
   const router = useRouter();
 
-  const handleClick = async () => {
+  const handleSubmit = async () => {
     try {
       const payload = { email, password };
+      const response = await loginApi(payload);
 
-      if (isLoginMode) {
-        const response = await loginApi(payload);
-        setCookie("user-access", JSON.stringify(response.data), {
-          expires: 1,
-        });
-      } else {
-        await registerApi(payload);
-      }
+      // Assuming the API response contains the user data
+      const userData = response.data;
 
+      // Set user-access cookie
+      setCookie("user-access", JSON.stringify(userData), {
+        expires: 1,
+      });
+
+      // Redirect to the home page
       router.push("/").then(() => router.reload());
     } catch (error) {
-      console.error("Login or register error:", error);
-      setError("Invalid email or password");
+      console.error("Login failed:", error);
     }
   };
 
-  const toggleMode = () => {
-    setLoginMode((prevMode) => !prevMode);
-    setError(""); // Clear error on mode switch
+  const switchForm = () => {
+    // Toggle between login and registration forms
+    setIsLogin((prev) => !prev);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
-        <h2 style={{ color: "white" }}>{isLoginMode ? "Login" : "Register"}</h2>
-        <input
-          type="text"
-          className={styles.inputField}
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          className={styles.inputField}
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        {error && <p className={styles.errorMsg}>{error}</p>}
-        <button className={styles.submitButton} onClick={handleClick}>
-          {isLoginMode ? "Login" : "Register"}
-        </button>
-        <p style={{ color: "white", margin: "10px 0" }}>
-          {isLoginMode ? "Not a user? " : "Already a user? "}
-          <button className={styles.switchButton} onClick={toggleMode}>
-            {isLoginMode ? "Register now" : "Login now"}
+        <h1>{isLogin ? "Login" : "Register"}</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={styles.inputField}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={styles.inputField}
+          />
+          <button type="submit" className={styles.submitButton}>
+            {isLogin ? "Login" : "Register"}
           </button>
+        </form>
+        <p className={styles.switchButton} onClick={switchForm}>
+          {isLogin ? "Not a user? Register now" : "Already a user? Login now"}
         </p>
       </div>
     </div>
