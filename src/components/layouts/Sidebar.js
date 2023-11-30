59fcabfde855be51b06f6e5614e7b5bf31b5347e
Evander Oktapian <evandertambunan@gmail.com>
2023-11-30 14:31:36 +0700

    tes

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -1,62 +1,63 @@
 import { IconContext } from "react-icons";
 import styles from "./sidebar.module.css";
-import {
-  MdOutlineSpaceDashboard,
-  MdOutlineTrendingDown,
-  MdOutlineTrendingUp,
-  MdWallet,
-  MdPersonOutline,
-} from "react-icons/md";
-import WalletSummary from "../elements/WalletSummary";
-import ExpenseForm from "../elements/asdasd";
-import IncomeForm from "../elements/ModalIncomeForm";
+import { MdOutlineSpaceDashboard, MdOutlineTrendingDown, MdOutlineTrendingUp, MdWallet, MdPersonOutline } from "react-icons/md";
 import Link from "next/link";
+import { Button } from "react-bootstrap";
+import { removeCookie } from "@/utils/cookies";
 
 const Sidebar = () => {
+    // const logOut = ()=>{
+    //     removeCookie('user-access')
+    //     router.reload("/login")
+    //   }
+
     return (
         <>
-            <div className={styles.container}>
-                <div className={styles.sidebar}>
-                    <div className={styles.sidebarItem}>
+            <div className={` ${styles.container}`}>
+                <div className={`position-relative ${styles.sidebar}`}>
+                    <div className={`d-flex justify-content-center${styles.sidebarItem}`}>
                         <ul>
-                            <Link href="/dashboard">
-                                <IconContext.Provider value={{ color: "white", size: "2rem" }}>
-                                    <MdOutlineSpaceDashboard className={styles.icons} />
-                                </IconContext.Provider>
-                                Dashboard
+                            <Link href="/dashboard" className={styles.link}>
+                                <li className={styles.list}>
+                                    <IconContext.Provider value={{ color: "white", size: "2rem" }}>
+                                        <MdOutlineSpaceDashboard className={styles.icons} />
+                                    </IconContext.Provider>
+                                    Dashboard
+                                </li>
                             </Link>
-                        </ul>
-                        <ul>
-                            <Link href="/expanses">
-                                <IconContext.Provider value={{ color: "white", size: "2rem" }}>
-                                    <MdOutlineTrendingDown className={styles.icons} />
-                                </IconContext.Provider>
-                                Expenses
+                            <Link href="/expanses" className={styles.link}>
+                                <li className={styles.list}>
+                                    <IconContext.Provider value={{ color: "white", size: "2rem" }}>
+                                        <MdOutlineTrendingDown className={styles.icons} />
+                                    </IconContext.Provider>
+                                    Expenses
+                                </li>
                             </Link>
-                        </ul>
-                        <ul>
-                            <Link href="/income">
-                                <IconContext.Provider value={{ color: "white", size: "2rem" }}>
-                                    <MdOutlineTrendingUp className={styles.icons} />
-                                </IconContext.Provider>
-                                Incomes
+                            <Link href="/income" className={styles.link}>
+                                <li className={styles.list}>
+                                    <IconContext.Provider value={{ color: "white", size: "2rem" }}>
+                                        <MdOutlineTrendingUp className={styles.icons} />
+                                    </IconContext.Provider>
+                                    Incomes
+                                </li>
                             </Link>
-                        </ul>
-                        <ul>
-                            <Link href="/wallet">
-                                <IconContext.Provider value={{ color: "white", size: "2rem" }}>
-                                    <MdWallet className={styles.icons} />
-                                </IconContext.Provider>
-                                Wallet
+                            <Link href="/wallet" className={styles.link}>
+                                <li className={styles.list}>
+                                    <IconContext.Provider value={{ color: "white", size: "2rem" }}>
+                                        <MdWallet className={styles.icons} />
+                                    </IconContext.Provider>
+                                    Wallet
+                                </li>
                             </Link>
-                        </ul>
-                        <ul>
-                            <Link href="/profile">
-                                <IconContext.Provider value={{ color: "white", size: "2rem" }}>
-                                    <MdPersonOutline className={styles.icons} />
-                                </IconContext.Provider>
-                                Profile
+                            <Link href="/profile" className={styles.link}>
+                                <li className={styles.list}>
+                                    <IconContext.Provider value={{ color: "white", size: "2rem" }}>
+                                        <MdPersonOutline className={styles.icons} />
+                                    </IconContext.Provider>
+                                    Profile
+                                </li>
                             </Link>
+                            <Button className={`position-absolute top-50 start-50 translate-middle mt-5 ${styles.button}`}>Log-Out</Button>
                         </ul>
                     </div>
                 </div>
