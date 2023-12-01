587b4404067d29b97fd0f7434f9ae19f983e679d
Evander Oktapian <evandertambunan@gmail.com>
2023-12-01 17:19:02 +0700

    create animation for login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,6 +3,7 @@ import { setCookie } from "@/utils/cookies";
 import { Router, useRouter } from "next/router";
 import { useState } from "react";
 import styles from "./login.module.css"; // Sesuaikan dengan nama file CSS module yang benar
+import { useEffect } from "react";
 
 const login = () => {
     const [isChecked, setIsChecked] = useState(true);
@@ -10,6 +11,8 @@ const login = () => {
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
     const [confirmPassword,setConfirmPassword] = useState("")
+    const [errMessage,setErrMessage] = useState("")
+    const [isError,setIsError] = useState(false)
 
 
     const router = useRouter()
@@ -18,19 +21,30 @@ const login = () => {
             const payload = {email,password}
             const response = await loginApi(payload)
             const dataCookie = {...response.data,token:response.token}
-            console.log(dataCookie);
             setCookie("user-access",JSON.stringify(dataCookie),{
                 expires : 1
             })
             router.push('/dashboard').then(() => router.reload());
         } catch (error) {
-            console.log(error);
+          if (error.response.status===401) {
+            setErrMessage("Wrong Email/Password")
+            setIsError(true)
+            setTimeout(()=>{
+              setIsError(false)
+            },5000)
+            setEmail("")
+            setPassword("")
+          }
         }
     }
 
     const handleRegister = async()=>{
       if (password!=confirmPassword) {
-        alert ("password tidak sama")
+        setErrMessage("Password doesnt match")
+        setIsError(true)
+        setTimeout(()=>{
+          setIsError(false)
+        },5000)
         return
       }
       try {
@@ -42,8 +56,11 @@ const login = () => {
           setPassword('')       
         } else{throw error}
       } catch (error) {
-        console.log(error);
-        alert("Email sudah digunakan,silahkan login")
+        setErrMessage("Email has already been registered")
+        setIsError(true)
+        setTimeout(()=>{
+          setIsError(false)
+        },5000)
       }
     }
     const isFormRegisterValid = ()=>{
@@ -66,8 +83,10 @@ const login = () => {
     };
 
     return (
-        <div className="position-absolute top-50 start-50 translate-middle">
-            <div className={styles.container}>
+        <div className={`position-relative ${styles.body}`}>
+          <h1 className={`${styles.errmes} ${isError&&styles.true}`}>{errMessage}</h1>
+              
+            <div className={`${styles.container}`}>
                 {isChecked ? (
                     <>
                         <div className={styles.signup1}>
@@ -86,7 +105,7 @@ const login = () => {
                                 Log in
                             </label>
                             <input type="email" name="email" placeholder="Email" required="" className={styles.input} />
-                            <input type="password" name="pswd" placeholder="Password" required="" className={styles.input} />
+                            <input type="password" name="pswd" placeholder="Password" required="" className={styles.input} value={password} onChange={(e)=>setPassword(e.target.value)} />
                             <button className={styles.button} disabled={isFormLoginValid()} >Log In</button>
                         </div>
                     </>
